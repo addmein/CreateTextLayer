7bb07570af36f2ea38e016249ae62382be7dc676
Olimpiu Hulea <olimpiu_hulea@yahoo.ro>
2016-03-15 17:32:47 +0100

    updates

--- Create_TEXT_Layer.jsx
+++ Create_TEXT_Layer.jsx
@@ -1,7 +1,7 @@
 ﻿//DESCRIPTION: Selects all text objects that have to be translated and moves them on TEXT layer
 // Create TEXT layer for translation
 //
-// Modified 2016-02-05
+// Modified 2016-03-15
 // Olimpiu Hulea
 // (+31) 657976413
 
@@ -17,12 +17,79 @@ function Main() {
         addTextLayer("TEXT");
         selectText();
         alert("Finished!");
+        $.writeln("Finished!");
         }
     else {
 		// No documents are open, so display an error message.
-		alert("No InDesign documents are open. Please open a document and try again.")
+		//alert("No InDesign documents are open. Please open a document and try again.")
+        var theFolder = Folder.selectDialog ("Select a folder");
+        
+        if (theFolder != null) {
+            forEachDescendantFile(theFolder, doStuffIfDocument);
+        }
 	}
 }
+
+function doStuff(document) {
+        unlockLayers ();
+        addTextLayer("TEXT");
+        selectText();
+        // alert("Finished!");
+    }
+
+function doStuffIfDocument(oFile) {
+    if ((matchExtension (oFile, 'ai')) || (matchExtension (oFile, 'eps'))) { // process .ai or .eps files
+        var document = app.open(oFile);
+        try {
+                doStuff(document);
+            }
+        finally {
+                exportEps(oFile);
+                document.close( SaveOptions.DONOTSAVECHANGES );
+                    }
+            }
+    }
+
+
+function exportEps(file) {
+    var newFile = new File(file);
+    var saveDoc = app.activeDocument;
+    var saveOpts = new EPSSaveOptions();
+    //saveOpts.cmykPostScript = true;
+    //saveOpts.embedAllFonts = true;
+    saveOpts.compatibility = Compatibility.ILLUSTRATOR15;
+    saveDoc.saveAs (file, saveOpts);
+}
+
+function forEachDescendantFile(folder, callback) {  
+    
+    var aChildren = folder.getFiles();
+    for (var i = 0; i < aChildren.length; i++) {
+        $.writeln("Processing file " + i);
+        $.writeln("Total files:" + aChildren.length);
+        var child = aChildren[i];
+        $.writeln("file " + aChildren[i]);
+        if (child instanceof File) {
+            callback(child);
+        }
+        else if (child instanceof Folder) {
+            this.forEachDescendantFile(child, callback);
+        }
+        else {
+            throw new Error("The object at \"" + child.fullName + "\" is a child of a folder and yet is not a file or folder.");
+        }
+    }
+    alert("Finished!");
+}
+
+function matchExtension(iFile, sExtension) {
+    sExtension = "." + sExtension.toLowerCase();
+    var displayName = iFile.displayName.toLowerCase();
+    if (displayName.length < sExtension.length) {
+        return false;
+        }
+    return displayName.slice(-sExtension.length) === sExtension;
+    }
 
 function selectText() {
     var textRefs = app.activeDocument.textFrames;
@@ -44,7 +111,7 @@ function selectText() {
                     num = num + 1;
                     }         
             }
-        $.writeln("Total only numbers: " + num);
+        //$.writeln("Total only numbers: " + num);
         }
     }
 
@@ -66,7 +133,7 @@ function moreLetters (str) {
 
 function isNotCode (str) {  // check that str is not a code
         if ( (str.length <  5) && (( isNaN(str.slice(1)) == false ) || ( isNaN(str.substring(0, str.length-1)) == false )) ) {
-            $.writeln("This is a code");
+            //$.writeln("This is a code");
             return false;
             }
         else return true;
@@ -74,7 +141,7 @@ function isNotCode (str) {  // check that str is not a code
 
 function isNotEsmCode (str) {  // check that str is not an Esm code
     if ( str.slice(-2) == "GB" ) {
-            $.writeln("ESM code");
+            //$.writeln("ESM code");
             return false;
         }
     else return true;
@@ -83,9 +150,14 @@ function isNotEsmCode (str) {  // check that str is not an Esm code
 function unlockLayers () {
     doc = app.activeDocument;
     var len = doc.layers.length;
-    $.writeln(len);
+    //$.writeln(len);
     
     for ( var i = 0; i < len; i++) {
             doc.layers[i].locked = false;
+            lname = doc.layers[i].name;
+            $.writeln(lname);
+            if ( lname == "TEXT") {
+                  doc.layers[i].name = "oldText";
+                }
         }
     }
